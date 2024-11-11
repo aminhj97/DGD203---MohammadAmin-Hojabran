1bebe297196251c55dc6c5251a9dbdd06a8e3646
aminhj97 <aminhojhabran200@gmail.com>
2024-11-11 21:38:23 +0300

    Add project files.

---
+++ MohammadAminHojabran_GameProgramming1_CarClass_20241111/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Car
+{   
+    //engine & transmission
+    public bool IsEngineOn;
+    public bool IsTransmissionAutomatic;
+    //fuel
+    public float FuelLevel;
+    public float Mileage;
+
+    public bool IsSportModel;
+    public bool IsConvertible;
+    // property of car
+    public int NumberOfDoors;
+    public int HorsePower;
+    public float TopSpeed;
+    public float Weight;
+    public int Year;
+}
